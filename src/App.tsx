1ea74366aa790b442879c30318237fd4b5e130cf
Abhisek Dutta <abhisek.dutta.507@gmail.com>
2025-02-14 01:39:00 +0530

    UI v0.0.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,27 @@
-import { Fragment } from 'react';
 import { CoreWebElement } from '@abhisek507/react-core-web-element';
-import './App.css'
 
+const elementConfig = {
+  header: {
+    visible: true,
+    heading: 'Header',
+  },
+  nav: {
+    visible: true,
+    title: 'Nav',
+  },
+  content: {
+    visible: true,
+    title: 'Content',
+  },
+  sidebar: {
+    visible: true,
+    title: 'Sidebar',
+  },
+};
 
 function App() {
   return (
-    <Fragment>
-      <CoreWebElement
-        header={{
-          visible: true,
-          heading: 'Header',
-        }}
-        nav={{
-          visible: true,
-          title: 'Nav',
-        }}
-        content={{
-          visible: true,
-          title: 'Content',
-        }}
-        sidebar={{
-          visible: true,
-          title: 'Sidebar',
-        }}
-      />
-    </Fragment>
+    <CoreWebElement {...elementConfig} />
   );
 }
 
